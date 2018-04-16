5980434c5773da3af9119812941f9d7bb7ad467b
mwoss <wos.mateusz16@gmail.com>
2018-04-16 02:35:23 +0200

    Small upadte

--- rabbitmq_task/src/main/java/doctor/DoctorMessageEmiter.java
+++ rabbitmq_task/src/main/java/doctor/DoctorMessageEmiter.java
@@ -1,10 +1,16 @@
 package doctor;
 
+import com.rabbitmq.client.Channel;
 import utils.MessageEmitter;
+
+
 
 public class DoctorMessageEmiter implements Runnable, MessageEmitter {
 
     private Thread emitter;
+
+    public DoctorMessageEmiter(Channel emitterChannel, String id) {
+    }
 
     public void run() {
 
--- rabbitmq_task/src/main/java/doctor/DoctorMessageListener.java
+++ rabbitmq_task/src/main/java/doctor/DoctorMessageListener.java
@@ -1,8 +1,14 @@
 package doctor;
 
+
+import com.rabbitmq.client.Channel;
+
 public class DoctorMessageListener implements utils.MessageListener {
 
+    public DoctorMessageListener(Channel listenerChannel, String id) {
+    }
 
+    // use JDK9 for private interface methods
     public void receiveMessage() {
 
     }
