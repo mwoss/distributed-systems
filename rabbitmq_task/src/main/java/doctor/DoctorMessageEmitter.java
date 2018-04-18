512d8fd1a1217289305337dea9051287e780b888
mwoss <wos.mateusz16@gmail.com>
2018-04-19 01:31:11 +0200

    Rabbitmq hospital taks done, fixed some bugs

--- rabbitmq_task/src/main/java/doctor/DoctorMessageEmitter.java
+++ rabbitmq_task/src/main/java/doctor/DoctorMessageEmitter.java
@@ -15,6 +15,7 @@ public class DoctorMessageEmitter implements Runnable, MessageEmitter {
     private String id;
     private BufferedReader bufferedReader;
 
+
     private Thread emitter;
 
     DoctorMessageEmitter(Channel emitterChannel, String id) {
@@ -26,7 +27,7 @@ public class DoctorMessageEmitter implements Runnable, MessageEmitter {
     public void run() {
         String message;
         String routingKey;
-        while(true){
+        while (true) {
             System.out.println("Enter message: ");
             try {
                 message = bufferedReader.readLine();
@@ -34,8 +35,8 @@ public class DoctorMessageEmitter implements Runnable, MessageEmitter {
                     break;
                 }
                 routingKey = ConstValues.ROUTING_KEY_TECHNICIAN + parseInput(message);
-                message = message + " " + id;
-                emitterChannel.basicPublish(ConstValues.EXCHANGE_NAME_OUT, routingKey, null, message.getBytes("UTF-8"));
+                message = ConstValues.DOCTOR + " " +  message + " " + id;
+                emitterChannel.basicPublish(ConstValues.EXCHANGE_NAME_COMMON, routingKey, null, message.getBytes("UTF-8"));
                 System.out.println("Sent: " + message);
             } catch (IOException | IllegalArgumentException e) {
                 System.out.println(e.getMessage());
@@ -50,11 +51,11 @@ public class DoctorMessageEmitter implements Runnable, MessageEmitter {
         }
     }
 
-    private String parseInput(String message){
+    private String parseInput(String message) {
         String[] data = message.split(" ");
-        if(data.length != 2)
+        if (data.length != 2)
             throw new IllegalArgumentException("Not enough arguments");
-        if(!Arrays.asList(ConstValues.INJURIES).contains(data[0]))
+        if (!Arrays.asList(ConstValues.INJURIES).contains(data[0]))
             throw new IllegalArgumentException("Unrecognized injury");
         return data[0];
     }
