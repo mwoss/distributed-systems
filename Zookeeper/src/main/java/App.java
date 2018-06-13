d6e21fe608226ae3b36aec9b77e9f1ba9882726b
mwoss <wos.mateusz16@gmail.com>
2018-06-13 22:53:51 +0200

    Zookeper demo done

---
+++ Zookeeper/src/main/java/App.java
@@ -0,0 +1,36 @@
+import logic.Executor;
+import org.apache.zookeeper.KeeperException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+    public static void main(String[] args) throws IOException, KeeperException {
+        if (args.length < 2) {
+            System.err.println("USAGE: Executor hostPort znode [args ...]");
+            System.exit(2);
+        }
+        String hostPort = args[0];
+        String znode = "znode_test";
+        String exec[] = new String[args.length - 1];
+        System.arraycopy(args, 1, exec, 0, exec.length);
+
+        Executor executor = new Executor(hostPort, znode, exec);
+        new Thread(() -> {
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                while (true) {
+                    switch (br.readLine()) {
+                        case "tree":
+                            executor.printDescendantsTree(znode);
+                            break;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        executor.run();
+    }
+}
