a9fb13f1431a2beda800ce487dbfd8c4c38184ff
Mateusz Woś <wos.mateusz16@gmail.com>
2018-06-14 02:34:20 +0200

    Zookeeper second verson, working better

--- Zookeeper/src/main/java/second_ver/Executor.java
+++ Zookeeper/src/main/java/second_ver/Executor.java
@@ -1,67 +1,90 @@
-package logic;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.List;
+package second_ver;
 
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
+import java.io.*;
+import java.util.List;
 
 public class Executor implements Watcher, Runnable, DataMonitorListener {
-
     private String znode;
     private DataMonitor dm;
     private ZooKeeper zk;
     private String exec[];
     private Process child;
 
-    public Executor(String hostPort, String znode, String exec[]) throws KeeperException, IOException {
-        this.exec = exec;
+    public Executor(String hostPort, String znode, String exec[]) throws KeeperException, IOException, InterruptedException {
         this.znode = znode;
-        zk = new ZooKeeper(hostPort, 3000, this);
+        this.exec = exec;
+        zk = new ZooKeeper(hostPort, 10000, this);
         dm = new DataMonitor(zk, znode, null, this);
     }
 
-    public void printDescendantsTree(String path) {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Start program with 2 or more args: hostPort program [args ...]");
+            System.exit(2);
+        }
+        String hostPort = args[0];
+        String znode = "/zknode-test";
+        String exec[] = new String[args.length - 1];
+        System.arraycopy(args, 1, exec, 0, exec.length);
         try {
-            if (zk.exists(path, this) != null) {
-                System.out.println(path);
-                List<String> children = zk.getChildren(path, this);
-                children.forEach(child -> printDescendantsTree(path + "/" + child));
-            }
-        } catch (KeeperException | InterruptedException e) {
+            new Executor(hostPort, znode, exec).run();
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void process(WatchedEvent event) {
+        dm.process(event);
     }
 
     public void run() {
         try {
-            synchronized (this) {
-                while (!dm.dead) {
-                    wait();
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                switch (br.readLine()) {
+                    case "tree":
+                        printTree(znode);
+                        break;
                 }
             }
-        } catch (InterruptedException e) {
+        } catch (IOException  e) {
             e.printStackTrace();
         }
     }
 
-    public void process(WatchedEvent watchedEvent) {
+    private void printTree(String path) {
+        try {
+            if (zk.exists(path, this) != null) {
+                System.out.println(path);
+                List<String> children = zk.getChildren(path, this);
+                children.forEach(child -> printTree(path + "/" + child));
+            }
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void closing(int rc) {
+        synchronized (this) {
+            notifyAll();
+        }
     }
 
     public void exists(byte[] data) {
         if (data == null) {
             if (child != null) {
                 System.out.println("Killing process");
+                child.destroyForcibly();
                 child.destroy();
                 try {
                     child.waitFor();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                } catch (InterruptedException ignored) {
                 }
             }
             child = null;
@@ -76,24 +99,20 @@ public class Executor implements Watcher, Runnable, DataMonitorListener {
                 }
             }
             try {
-                System.out.println("Starting child");
+                zk.getChildren(znode, this);
                 child = Runtime.getRuntime().exec(exec);
+                System.out.println("Starting child ");
                 new StreamWriter(child.getInputStream(), System.out);
                 new StreamWriter(child.getErrorStream(), System.err);
-            } catch (IOException e) {
+            } catch (IOException | InterruptedException | KeeperException e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public void closing(int rc) {
-        synchronized (this) {
-            notifyAll();
-        }
-    }
-
     static class StreamWriter extends Thread {
         OutputStream os;
+
         InputStream is;
 
         StreamWriter(InputStream is, OutputStream os) {
@@ -101,6 +120,7 @@ public class Executor implements Watcher, Runnable, DataMonitorListener {
             this.os = os;
             start();
         }
+
         public void run() {
             byte b[] = new byte[80];
             int rc;
@@ -108,9 +128,9 @@ public class Executor implements Watcher, Runnable, DataMonitorListener {
                 while ((rc = is.read(b)) > 0) {
                     os.write(b, 0, rc);
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+            } catch (IOException ignored) {
             }
+
         }
     }
 }
